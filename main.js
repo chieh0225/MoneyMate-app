6c8a69844cef5ade090c39a551bc973aba20741c
chieh <secret12501250@gmail.com>
2024-09-19 17:45:23 +0800

    refactor: 移除 main.js 中的 footer nav 部分

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.js";
 
-// 計算機.js
+// 計算機
 import "./assets/js/calculator.js";
 
 // linkState
-import './assets/js/linkState.js';
-// chart.js
+import "./assets/js/linkState.js";
+
+// chart
 import "./assets/js/custom-chart.js";
 
 // 載入圖片
@@ -49,59 +50,6 @@ document.addEventListener("DOMContentLoaded", () => {
       updateImageForDarkMode(); // 更新圖片
     });
   }
-});
-
-// footer nav
-window.addEventListener("DOMContentLoaded", () => {
-  // 選擇 navItems 和設置事件
-  const navItems = document.querySelectorAll(".nav-item");
-
-  // 當頁面載入時，根據 LocalStorage 的值設定樣式
-  const activeLink = localStorage.getItem("activeNavItem");
-  console.log("Active Link from LocalStorage:", activeLink); // 調試用
-  if (activeLink) {
-    const absoluteActiveLink = new URL(activeLink, window.location.origin).href;
-    console.log(
-      "Active Link from LocalStorage (Absolute):",
-      absoluteActiveLink
-    ); // 調試用
-
-    navItems.forEach((item) => {
-      const linkElement = item.querySelector("a");
-      if (linkElement) {
-        // 確保 href 值為絕對路徑進行比較
-        const link = new URL(
-          linkElement.getAttribute("href"),
-          window.location.origin
-        ).href;
-        console.log("Current link:", link); // 調試用
-        if (link === absoluteActiveLink) {
-          item.classList.add("active");
-        }
-      }
-    });
-  }
-
-  // 為每個 navItem 綁定點擊事件
-  navItems.forEach((item) => {
-    item.addEventListener("click", function () {
-      // 移除其他選項的 active 樣式
-      navItems.forEach((nav) => nav.classList.remove("active"));
-      // 為點選的項目添加 active 樣式
-      this.classList.add("active");
-
-      // 將選中的項目的 href 存入 LocalStorage
-      const linkElement = this.querySelector("a");
-      if (linkElement) {
-        const link = new URL(
-          linkElement.getAttribute("href"),
-          window.location.origin
-        ).href;
-        console.log("Saving Active Link to LocalStorage:", link); // 調試用
-        localStorage.setItem("activeNavItem", link);
-      }
-    });
-  });
 });
 
 // record.js
